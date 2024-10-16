f0d793f07859ac3a2ca66ac8e7a124405ebf81a1
Didier Loiseau <didierloiseau+git@gmail.com>
2024-10-16 18:57:17 +0200

    Unit test: parent with relative path shouldn’t be upgraded

--- rewrite-maven/src/test/java/org/openrewrite/maven/UpgradeParentVersionTest.java
+++ rewrite-maven/src/test/java/org/openrewrite/maven/UpgradeParentVersionTest.java
@@ -137,6 +137,33 @@ class UpgradeParentVersionTest implements RewriteTest {
     }
 
     @Test
+    void noUpgradeIfRelativePath() {
+        rewriteRun(
+          spec -> spec.recipe(new UpgradeParentVersion(
+            "org.springframework.boot",
+            "spring-boot-starter-parent",
+            "~1.5",
+            null
+          )),
+          pomXml(
+            """
+              <project>
+                <parent>
+                  <groupId>org.springframework.boot</groupId>
+                  <artifactId>spring-boot-starter-parent</artifactId>
+                  <version>1.5.12.RELEASE</version>
+                  <relativePath>..</relativePath>
+                </parent>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>my-app</artifactId>
+                <version>1</version>
+              </project>
+              """
+          )
+        );
+    }
+
+    @Test
     void upgradeToExactVersion() {
         rewriteRun(
           spec -> spec.recipe(new UpgradeParentVersion(
